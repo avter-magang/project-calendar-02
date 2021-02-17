03dbbde549d5403aa10925301ad9f1c47b95ddda
edwinfernaldy <edwinfernaldy@gmail.com>
2021-02-17 15:30:25 +0700

    calendar no 2 added

---
+++ date-helpers.js
@@ -0,0 +1,12 @@
+const isWeekend = day => {
+    // 6 = saturday, 0 = sunday
+    return day % 7 === 6 || day % 7 === 0;
+}
+
+const getDayName = day => {
+    const date = new Date(Date.UTC(2021, 0, day));
+
+    return new Intl.DateTimeFormat("en-US", { weekday: "short" }).format(date);
+}
+
+// export {isWeekend, getDayName}
